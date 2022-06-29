d356c914094d7cd8a6c35a6a58c765db6d00a7c5
hdlee <dev.fringe@gmail.com>
2022-06-30 00:10:57 +0900

    init

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,31 @@
-import {LitElement, html} from 'lit-element';
-import '@vaadin/vaadin-text-field/vaadin-text-field.js';
-import '@vaadin/vaadin-button/vaadin-button.js';
-import { customElement, state } from 'lit/decorators.js';
-import  '@vaadin/vaadin-lumo-styles/color.js';
-import  '@vaadin/vaadin-lumo-styles/typography.js';
-import  '@vaadin/vaadin-lumo-styles/sizing.js';
-import  '@vaadin/vaadin-lumo-styles/spacing.js';
-import  '@vaadin/vaadin-lumo-styles/style.js';
-import '@vaadin/vaadin-lumo-styles/typography.js';
+import { LitElement, html, query } from 'lit-element';
+import { customElement} from 'lit/decorators.js';
+import '@vaadin/vaadin-grid/vaadin-grid';
+import '@vaadin/vaadin-grid/vaadin-grid-column';
 
-// Import the <custom-style> element from Polymer and include
-// the style sheets in the global scope
-import '@polymer/polymer/lib/elements/custom-style.js';
-import  '@vaadin/vaadin-lumo-styles/presets/compact.js';
 @customElement('demo-app')
 export class DemoApp extends LitElement {
-    @state()
-    private counter = 0;
+
+    @query('#grid')
+    private grid: any;
+
+    firstUpdated() {
+        fetch(`https://demo.vaadin.com/demo-data/1.0/people?count=200`)
+            .then((r) => r.json())
+            .then((data) => {
+                this.grid.items = data.result;
+            });
+    }
 
     render() {
         return html`
-        <vaadin-horizontal-layout theme="spacing" style="align-items: baseline">
-        <vaadin-button @click="${() => this.counter++}">Button</vaadin-button>
-        <p>Clicked ${this.counter} times</p>
-      </vaadin-horizontal-layout>
-      <vaadin-horizontal-layout theme="spacing">
-      <vaadin-button theme="primary">Primary</vaadin-button>
-      <vaadin-button theme="secondary">Secondary</vaadin-button>
-      <vaadin-button theme="tertiary">Tertiary</vaadin-button>
-    </vaadin-horizontal-layout>   
+            <div>
+                <vaadin-grid id="grid">
+                    <vaadin-grid-column header="First name" path="firstName"></vaadin-grid-column>
+                    <vaadin-grid-column header="Last name" path="lastName"></vaadin-grid-column>
+                    <vaadin-grid-column header="Email" path="email"></vaadin-grid-column>
+                </vaadin-grid>
+            </div>
         `;
     }
 }
